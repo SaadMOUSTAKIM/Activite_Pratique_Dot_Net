e909d1c697f78261f75af1e9d9263f23b1313509
Saad MOUSTAKIM <saadmoustakim2@gmail.com>
2022-12-16 20:44:13 +0100

    console app

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Service;
+
+AccountService accS = new AccountServiceImpl();
+accS.AddNewAccount(1, "MAD", 5000);
+accS.AddNewAccount(2, "MAD", -500);
+
+
+List<Account> listAccounts = accS.GetAllAccounts();
+foreach (Account acc in listAccounts)
+{
+    Console.WriteLine(acc);
+}
+Console.WriteLine("---------Debit Account-------------");
+List<Account> DebitedAccounts = accS.GetDebitedAccounts();
+foreach (Account acc in DebitedAccounts)
+{
+    Console.WriteLine(acc);
+}
+Console.WriteLine("----------------------------------------");
+Console.WriteLine(accS.GetBalanceAVG());
